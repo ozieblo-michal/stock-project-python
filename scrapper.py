0ad29b50f6e9412c6604349bce5e18c014f80bb9
ozieblo-repository <59211762+ozieblo-repository@users.noreply.github.com>
2020-07-11 00:34:58 +0200

    demo of the scrapper

---
+++ scrapper.py
@@ -0,0 +1,30 @@
+# download .zip file with raw data
+
+import urllib.request
+class AppURLopener(urllib.request.FancyURLopener):
+    version = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.69 Safari/537.36"
+urllib._urlopener = AppURLopener()
+urllib._urlopener.retrieve("https://static.VENDOR-NAME.pl/db/h/d_pl_txt.zip", # fill
+                           "d_pl_txt.zip")
+
+# unzip only needed files
+
+import zipfile
+
+archive = zipfile.ZipFile('d_pl_txt.zip')
+
+for file in archive.namelist():
+    if file.startswith('data/daily/pl/wse stocks/'):
+        archive.extract(file,
+                        '/Users/USER-NAME/Desktop/wse-dash/wseStocks') # fill
+
+# delete used .zip file
+
+import os
+
+myfile = "d_pl_txt.zip"
+
+try:
+    os.remove(myfile)
+except OSError as e:
+    print ("Error: %s - %s." % (e.filename, e.strerror))
