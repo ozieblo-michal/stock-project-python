f900e2123d6382313c295c5baee11803279109c6
ozieblo-repository <ozieblo.michal@icloud.com>
2020-07-19 20:11:50 +0200

    RSI update

--- wse-dash/apps/contact.py
+++ wse-dash/apps/contact.py
@@ -27,11 +27,12 @@ layout = html.Div([
             dbc.Col(
                 [html.H2("Mateusz Jęczarek"),
                  html.P("""\
-                            index number: 89338
-                        """),
-                 html.P("""\
-                            e-mail adress: mateusz.jeczarek@gmail.com
-                            phone number: 728-326-989
+                            Donec id elit non mi porta gravida at eget metus.Fusce dapibus, tellus ac cursus
+                            commodo, tortor mauris condimentumnibh, ut fermentum massa justo sit amet risus
+                            . Etiam porta semmalesuada magna mollis euismod. Donec sed odio dui. Donec id
+                            elit nonmi porta gravida at eget metus. Fusce dapibus, tellus ac cursuscommodo,
+                            tortor mauris condimentum nibh, ut fermentum massa justo sitamet risus. Etiam
+                            porta sem malesuada magna mollis euismod. Donec sedodio dui.
                         """),
                  ])
             ])
--- wse-dash/databases/wsedfIntoDict.py
+++ wse-dash/databases/wsedfIntoDict.py
@@ -1,7 +1,5 @@
 import pandas as pd
 
-from math import isnan
-
 # spolki z infosfera
 
 class KmeanOptions:
@@ -11,9 +9,11 @@ class KmeanOptions:
 
     def wig20_options_for_kmean(self):
 
-        wig20_df = pd.read_csv('/Users/mateuszjeczarek/PycharmProjects/stock-project-python-new_master/wse-dash/databases/wse_wig20.csv', delimiter=";")
+        wig20_df = pd.read_csv('/Users/michalozieblo/Desktop/wse-dash/databases/wse_wig20.csv',
+                               delimiter=";")
 
-        wig20Abbrev = dict(zip(wig20_df['Nazwa giełdowa'], wig20_df['Ticker']))
+        wig20Abbrev = dict(zip(wig20_df['Nazwa giełdowa'],
+                               wig20_df['Ticker']))
 
         wig20_options_for_kmean = []
 
@@ -27,9 +27,11 @@ class KmeanOptions:
 
     def mwig40_options_for_kmean(self):
 
-        mwig40_df = pd.read_csv('/Users/mateuszjeczarek/PycharmProjects/stock-project-python-new_master/wse-dash/databases/wse_mwig40.csv', delimiter=";")
+        mwig40_df = pd.read_csv('/Users/michalozieblo/Desktop/wse-dash/databases/wse_mwig40.csv',
+                                delimiter=";")
 
-        mwig40Abbrev = dict(zip(mwig40_df['Nazwa giełdowa'], mwig40_df['Ticker']))
+        mwig40Abbrev = dict(zip(mwig40_df['Nazwa giełdowa'],
+                                mwig40_df['Ticker']))
 
         mwig40_options_for_kmean = []
 
@@ -43,9 +45,11 @@ class KmeanOptions:
 
     def swig80_options_for_kmean(self):
 
-        swig80_df = pd.read_csv('/Users/mateuszjeczarek/PycharmProjects/stock-project-python-new_master/wse-dash/databases/wse_swig80.csv', delimiter=";")
+        swig80_df = pd.read_csv('/Users/michalozieblo/Desktop/wse-dash/databases/wse_swig80.csv',
+                                delimiter=";")
 
-        swig80Abbrev = dict(zip(swig80_df['Nazwa giełdowa'], swig80_df['Ticker']))
+        swig80Abbrev = dict(zip(swig80_df['Nazwa giełdowa'],
+                                swig80_df['Ticker']))
 
         swig80_options_for_kmean = []
 
@@ -56,3 +60,21 @@ class KmeanOptions:
             swig80_options_for_kmean.append(tmp_dict_3)
 
         return swig80_options_for_kmean
+
+    def wse_options_for_indicators(self):
+
+        wse_df = pd.read_csv('/Users/michalozieblo/Desktop/wse-dash/databases/wseDataframe.csv',
+                             delimiter=";")
+
+        wseAbbrev = dict(zip(wse_df['Nazwa giełdowa'],
+                             wse_df['Ticker']))
+
+        wse_options_for_kmean = []
+
+        for key, value in wseAbbrev.items():
+            tmp_dict_1 = {'label': key}
+            tmp_dict_2 = {'value': value}
+            tmp_dict_3 = {**tmp_dict_1, **tmp_dict_2}
+            wse_options_for_kmean.append(tmp_dict_3)
+
+        return wse_options_for_kmean
