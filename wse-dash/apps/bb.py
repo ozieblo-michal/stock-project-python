d0f5ec2a867386dad13c1f2816a45ec053f5d486
Mattyama91 <61246920+Mattyama91@users.noreply.github.com>
2020-07-24 13:20:32 +0200

    bb update

--- wse-dash/apps/bb.py
+++ wse-dash/apps/bb.py
@@ -15,49 +15,34 @@ layout = html.Div([
                          width={"size": 6, "offset": 3}),
                  id="bb-row-0"),
          dbc.Row(dbc.Col(html.Div(html.H1("BOLLINGER BANDS")),
-                         width={"size": 6, "offset": 3}),
+                         width={"size": 6, "offset": 4}),
                  id="bb-row-1"),
-         dbc.Row(dbc.Col(html.Div(html.H3("Description:")),
-                         width={"size": 6, "offset": 3}),
-                 id="bb-row-2"),
-         dbc.Row(dbc.Col(html.Div("""\
-                                    The set consists of three elements. The first (and also the middle, if
-                                    you look at the resulting whole plot) is the moving average (SMA) with
-                                    a default value of 20. Two other components to the control lines, which
-                                    are drawn below and superior. Lower band to SMA minus two standard deviations.
-                                    Combined top band for SMA plus two standard deviations. There are opinions
-                                    that the standard value deviation is 2.5 thanks to which 99% of the price
-                                    action is between two ribbons, which means that the external injection
-                                    becomes a very significant signal.
-                                    """),
-                         width={"size": 6, "offset": 3}),
-                 id="bb-row-3"),
-         dbc.Row(dbc.Col(html.Div(html.H4("Select the company to Bollinger Bands analyze:")),
-                         width={"size": 6, "offset": 3}),
-                 id="bb-row-4"),
+         dbc.Row(dbc.Col([html.Div(
+         "The set consists of three elements. The first (and also the middle, if \
+         you look at the resulting whole plot) is the moving average (SMA) with \
+         a default value of 20. Two other components to the control lines, which \
+         are drawn below and superior. Lower band to SMA minus two standard deviations. \
+         Combined top band for SMA plus two standard deviations. There are opinions \
+         that the standard value deviation is 2.5 thanks to which 99% of the price \
+         action is between two ribbons, which means that the external injection \
+         becomes a very significant signal."
+         ),
+         html.P("")])),
+        dbc.Row(dbc.Col(html.Div("Select the company to analyze:"),
+                                 width={"size": 6, "offset": 3}),
+                         id="bb-row-2"),
          dbc.Row(dbc.Col(html.Div([
                             dcc.Dropdown(id='dropdown-so',
                                          options=KmeanOptions().wse_options_for_indicators(),
-                                         value='11B'),
-             html.Div(id='dd-output-container')]),
+                                         value='11B')
+                                  ]),
              width={"size": 6, "offset": 3}),
-             id="bb-row-5"),
-         dbc.Row(dbc.Col(dbc.Button("Analyze",
-                                    color="primary",
-                                    block=True),
-                         width={"size": 6, "offset": 3}),
-                 id="bb-row-6"),
-         dbc.Row(dbc.Col(html.Div("Results"),
+             id="bb-row-3"),
+         dbc.Row(dbc.Col([html.Div([dcc.Graph(id='bb-plot'),
+                                    ])]),
+                 id="bb-row-4"),
+         dbc.Row(dbc.Col([html.Div(id="update-table")],
                          width={"size": 6,
                                 "offset": 3}),
-                 id="bb-row-7"),
-         dbc.Row(dbc.Col(html.Div(html.H2(dcc.Graph(id='bb-plot')))),
-                 id="bb-row-8"),
-         dbc.Row(dbc.Col(dbc.Button("Export the report",
-                                    color="primary",
-                                    block=True),
-                         width={"size": 6,
-                                "offset": 3}),
-                 id="bb-row-9")]
-    )
-])
+                 id="bb-row-5")
+         ])])
